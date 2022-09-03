13b2591d0029fc595b0fb6c4bbe4d1cda0eca3ae
Ivo Jose <ivojmgs19@gmail.com>
2022-09-03 20:52:16 -0300

    Protegendo apliacao contra serializacao

--- src/main/java/com/ivogoncalves/helpdesk/domain/Tecnico.java
+++ src/main/java/com/ivogoncalves/helpdesk/domain/Tecnico.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ivogoncalves.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -15,6 +16,7 @@ public class Tecnico extends Pessoa {
 	private static final long serialVersionUID = 1L;
 	
 	@OneToMany(mappedBy = "tecnico")
+	@JsonIgnore
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Tecnico() {
