254a10a9fe684c5a90eb332ba6ec91ed95f947e3
Ivo Jose <ivojmgs19@gmail.com>
2022-09-21 18:40:29 -0300

    Criacao do findById para Chamado

---
+++ src/main/java/com/ivogoncalves/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.ivogoncalves.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ivogoncalves.helpdesk.domain.Chamado;
+import com.ivogoncalves.helpdesk.repositories.ChamadoRepository;
+import com.ivogoncalves.helpdesk.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ChamadoService {
+
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	public Chamado findByID(Integer id) {
+		Optional<Chamado> obj = chamadoRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontrado! Id: " + id));
+	}
+}
