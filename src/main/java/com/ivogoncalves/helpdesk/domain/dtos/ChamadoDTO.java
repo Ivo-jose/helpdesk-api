57182b01a5acd836325f6b0d5bd0e74b10051d71
Ivo Jose <ivojmgs19@gmail.com>
2022-09-28 15:45:03 -0300

    Create para chamado

--- src/main/java/com/ivogoncalves/helpdesk/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/ivogoncalves/helpdesk/domain/dtos/ChamadoDTO.java
@@ -3,6 +3,8 @@ package com.ivogoncalves.helpdesk.domain.dtos;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ivogoncalves.helpdesk.domain.Chamado;
 
@@ -15,14 +17,19 @@ public class ChamadoDTO implements Serializable {
 	private LocalDate dataAbertura = LocalDate.now();
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+	@NotNull(message = "O campo PRIORIDADE é requerido")
 	private Integer prioridade;
+	@NotNull(message = "O campo STATUS é requerido")
 	private Integer status;
+	@NotNull(message = "O campo TITULO é requerido")
 	private String titulo;
+	@NotNull(message = "O campo OBESERVACOES é requerido")
 	private String observacoes;
-	
-	
+	@NotNull(message = "O campo TECNICO é requerido")
 	private Integer tecnico;
+	@NotNull(message = "O campo CLIENTE é requerido")
 	private Integer cliente;
+	
 	private String nomeTecnico;
 	private String nomeCliente;
 	
